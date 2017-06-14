f5ce749af25aab7f8d1b1348d8de8e3789ecd1c0
menxin.wu <memgxin.wu@qq.com>
2017-06-14 20:17:25 +0800

    completed

--- js/global.js
+++ js/global.js
@@ -1,4 +1,4 @@
-function highlightPage() {
+function highlightPage(){
 	$("header nav a").each(function(){
 		if(window.location.href.indexOf($(this).attr("href")) !== -1){
 			$(this).addClass("here");
@@ -6,38 +6,25 @@ function highlightPage() {
 		}
 	});
 }
-
-function moveElement(elementId,finalX,finalY,interval) {
-	var elem = $("#"+elementId)[0];
-	if(elem.movement) clearTimeout(elem.movement);
-	if(!elem.style.left) elem.style.left = "0px";
-	if(!elem.style.top) elem.style.top = "0px";
-	var xpos = parseInt(elem.style.left);
-	var ypos = parseInt(elem.style.top);
-	if(xpos === finalX && ypos === finalY){ return true; }
-	if(xpos < finalX){ xpos += Math.ceil((finalX - xpos)/10); }
-	if(xpos > finalX){ xpos -= Math.ceil((xpos - finalX)/10); }
-	if(ypos < finalY){ ypos += Math.ceil((finalY - ypos)/10); }
-	if(ypos > finalY){ ypos -= Math.ceil((ypos - finalY)/10); }
-	elem.style.left = xpos + "px";
-	elem.style.top = ypos + "px";
-	var repeat = "moveElement('" + elementId + "'," + finalX + "," + finalY + "," + interval + ")";
-	elem.movement = setTimeout(repeat,interval);
+function moveElement($element, finalX, interval){
+	if(!$element.is(":animated")){
+		$element.animate({left: finalX}, interval);
+	}
 }
-
-function prepareSlideShow() {
-	var $slideshow = $("<div></div>").attr("id","slideshow");
+function prepareSlideShow(){
+	var $slideshow = $("<div></div>").attr("id", "slideshow");
 	var $frame = $("<img>").attr({"src": "imgs/frame.gif", "alt": "", "id": "frame"});
 	var $preview = $("<img>").attr({"src": "imgs/slideshow.gif", "alt": "slideshow", "id": "preview"});
 	$slideshow.append($frame, $preview);
 	$("#intro").after($slideshow);
 	$("a").each(function(){
 		$(this).mouseover(function(){
-			if($(this).attr("href").indexOf("index") !== -1) {moveElement("preview",0,0,5)};
-			if($(this).attr("href").indexOf("about") !== -1) {moveElement("preview",-150,0,5)};
-			if($(this).attr("href").indexOf("photos") !== -1) {moveElement("preview",-300,0,5)};
-			if($(this).attr("href").indexOf("live") !== -1) {moveElement("preview",-450,0,5)};
-			if($(this).attr("href").indexOf("contact") !== -1) {moveElement("preview",-600,0,5)};
+			destination = $(this).attr("href");
+			if(destination.indexOf("index")   !== -1){ moveElement($preview, 0, 100); }
+			if(destination.indexOf("about")   !== -1){ moveElement($preview, -150, 100); }
+			if(destination.indexOf("photos")  !== -1){ moveElement($preview, -300, 100); }	
+			if(destination.indexOf("live")    !== -1){ moveElement($preview, -450, 100); }
+			if(destination.indexOf("contact") !== -1){ moveElement($preview, -600, 100); }
 		});
 	});
 }
@@ -52,7 +39,7 @@ function prepareInternalnav(){
 	$("article nav a").each(function(){
 		var sectionId = $(this).attr("href").split("#")[1];
 		$("#" + sectionId).hide();
-		$(this).click(function(){ showSection(sectionId); return false;});
+		$(this).click(function(event){ showSection(sectionId); event.preventDefault();});
 	});
 }
 
@@ -88,22 +75,14 @@ function resetFields(whichform){
 	for(var i=0; i<elements.length; i++){
 		var element = elements[i];
 		if(element.type === "submit") continue;
-		var check = element.placeholder || element.getAttribute("placeholder");
-		if(!check) continue;
-		element.onfocus = function(){
-			var text = this.placeholder || this.getAttribute("placeholder");
-			if(this.value === text){
-				this.className = "";
-				this.value = "";
-			}
-		};
-		element.onblur = function(){
-			if(this.value === ""){
-				this.className = "placeholder";
-				this.value = this.placeholder || this.getAttribute("placeholder");
-			}
-		};
-		element.onblur();
+		if(!$(element).attr("placeholder")) continue;
+		$(element).focus(function(){
+			var text = $(this).attr("placeholder");
+			if($(this).val() === text){ $(this).attr("class","").val(""); }		
+		}).blur(function(){
+			if($(this).val() === ""){ $(this).attr("class","placeholder").val($(this).attr("placeholder")); }
+		});
+		$(element).blur();
 	}
 }
 
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 1. 2017/6/11: first version, example01 js use jQuery.
 2. 2017/6/13: second version, realize the form with jQuery Ajax.
+3. 2017/6/14: third version, completed the website:example01 with jQuery.
